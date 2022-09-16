9310d5b73730da1f7053cb2cb1311522417a02b4
fernandoareias <nando.calheirosx@gmail.com>
2022-09-15 22:51:50 -0300

    [ANGULAR] - Implementação do Guard

---
+++ src/web-application/src/app/core/guards/auth.guard.ts
@@ -0,0 +1,24 @@
+import { Injectable } from '@angular/core';
+import { ActivatedRouteSnapshot, CanActivate, Router, RouterStateSnapshot, UrlTree } from '@angular/router';
+import { Observable } from 'rxjs';
+import { AuthService } from '../services/auth.service';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthGuard implements CanActivate {
+
+  constructor(public router: Router,
+    public authServuce: AuthService) {
+
+
+  }
+  canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): boolean | UrlTree | Observable<boolean | UrlTree> | Promise<boolean | UrlTree> {
+    if(!this.authServuce.isAuthenticated()){
+      this.router.navigate(['auth/login']);
+      return false;
+    }
+    return true;
+  }
+//access-token
+}
